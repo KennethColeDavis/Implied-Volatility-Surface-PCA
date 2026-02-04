780e11475cfa621d2a3c270c9f110a70e0cd781f
Kenneth "Cole" Davis <kcda230@uky.edu>
2026-02-04 17:10:44 -0500

    Just changed function name of root finder to be more practical

--- src/implied_vol.py
+++ src/implied_vol.py
@@ -15,12 +15,12 @@ def implied_volatility(price, S, K, T, r, option_type, sigma_low=.00001, sigma_h
     option_type = option_type.lower().strip()
 
     # Define the root-finding target: BS_price(sigma) - market_price = 0
-    def objective(sigma):
+    def findIV(sigma):
         return black_scholes_price(S, K, T, r, sigma, option_type=option_type) - price
 
     # Find sigma such that objective(sigma)=0 using a safe bracketed method
     try:
-        return brentq(objective, sigma_low, sigma_high, maxiter=500)       #brentq is a root-finding algorithm that combines bisection, secant, and inverse quadratic interpolation methodds
+        return brentq(findIV, sigma_low, sigma_high, maxiter=500)       #brentq is a root-finding algorithm that combines bisection, secant, and inverse quadratic interpolation methodds
     except ValueError:
         # Means the root was not bracketed in [sigma_low, sigma_high]
         return np.nan
