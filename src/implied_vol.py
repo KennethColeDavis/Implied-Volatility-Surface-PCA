efe32c8b4fac7860a1fb739b55d412dd37b3dd92
Kenneth "Cole" Davis <kcda230@uky.edu>
2026-02-04 17:05:41 -0500

    Add IV script to calulte IV using BS and brentq root finder

---
+++ src/implied_vol.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy.optimize import brentq
+
+from src.black_scholes import black_scholes_price       # Maybe from .black_scholes?
+
+#############################################################################################################################
+#Here we solve for implied volatility (sigma) given a market option price by reverse engineering the Black–Scholes formula.
+#Returns np.nan if inputs are invalid or the implied vol cannot be found within bounds.
+#############################################################################################################################ß
+
+
+def implied_volatility(price, S, K, T, r, option_type, sigma_low=.00001, sigma_high=5.0):   # arbitrary bounds for volatility
+
+    # Standardize option type input
+    option_type = option_type.lower().strip()
+
+    # Define the root-finding target: BS_price(sigma) - market_price = 0
+    def objective(sigma):
+        return black_scholes_price(S, K, T, r, sigma, option_type=option_type) - price
+
+    # Find sigma such that objective(sigma)=0 using a safe bracketed method
+    try:
+        return brentq(objective, sigma_low, sigma_high, maxiter=500)       #brentq is a root-finding algorithm that combines bisection, secant, and inverse quadratic interpolation methodds
+    except ValueError:
+        # Means the root was not bracketed in [sigma_low, sigma_high]
+        return np.nan
